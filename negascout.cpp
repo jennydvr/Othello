44a0438b4032d0cf43643f56c6890b3a3453d9df
gracegimon <gracegimon@gmail.com>
2012-10-29 17:20:34 -0430

    Negascout modificado

--- negascout.cpp
+++ negascout.cpp
@@ -6,7 +6,7 @@
 //
 //
 #include "negascout.h"
-
+using namespace std;
 int negascout(state_t node, int depth, int alpha, int beta, bool color){
 	   if (node.terminal() || depth==0)
 	        return node.value();
@@ -20,8 +20,14 @@ int negascout(state_t node, int depth, int alpha, int beta, bool color){
            else if (!color && node.is_white_move(pos)){
         	   child = node.white_move(pos);
            }
+           else{
+        	   continue;
+           }
+           cout << "child " << endl << child;
+           cout << endl;
     	   int a = -negascout (child, depth-1, -b, -alpha, !color);
-   	       if (a>alpha) alpha = a;
+   	       if (a>alpha) 
+   	    	   alpha = a;
    	       if (alpha>=beta)
    	           return alpha; //Beta pruning
    	       if (alpha>=b){
