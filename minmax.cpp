b95bcb62c647113ac0a2f9008548bbc47bb614ca
Jenny Valdez <jedaniv@gmail.com>
2012-11-01 17:29:11 -0430

    Mod a minmax

--- minmax.cpp
+++ minmax.cpp
@@ -8,17 +8,34 @@
 
 #include "minmax.h"
 
+using namespace std;
+
+vector<state_t> getChildren(state_t node, bool color) {
+    vector<state_t> children;
+    
+    // Calculate all possible plies
+    for (int pos = 0; pos < DIM; ++pos)
+        if (color && node.is_black_move(pos))
+            children.push_back(node.black_move(pos));
+        else if (!color && node.is_white_move(pos))
+            children.push_back(node.white_move(pos));
+    
+    // If there are none, pass
+    if (children.empty())
+        children.push_back(node);
+    
+    return children;
+}
+
 int minmax(state_t node, int depth, bool color) {
     if (depth == 0 || node.terminal())
         return node.value();
     
     int alpha = INT_MIN;
+    vector<state_t> children = getChildren(node, color);
     
-    for (int pos = 0; pos < DIM; ++pos) {
-        if (color && node.is_black_move(pos))
-            alpha = MAX(alpha, -minmax(node.black_move(pos), depth - 1, !color));
-        else if (!color && node.is_white_move(pos))
-            alpha = MAX(alpha, -minmax(node.white_move(pos), depth - 1, !color));
-    }
+    for (int i = 0; i != children.size(); ++i)
+        alpha = MAX(alpha, -minmax(children[i], depth -1, !color));
+    
     return alpha;
 }
