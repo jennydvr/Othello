575bdd8b2820906058ab05ed86dc1e81cca7799d
gracegimon <gracegimon@gmail.com>
2012-11-04 22:51:28 -0430

    Negascout con table transposition

--- negascout.cpp
+++ negascout.cpp
@@ -14,20 +14,39 @@ int negaMin(state_t state, int depth, int alpha, int beta, bool player);
 
 int negaMax(state_t state, int depth, int alpha, int beta, bool player) {
     hash_table_t::iterator it = negascout_table.find(state);
-    
-    if (it != negascout_table.end())
-        return (it->second).val;
+    int value, d, type;
+    if (it != negascout_table.end()){
+	value = (it->second).val; 
+        d = (it->second).depth;
+	type = (it->second).type;
+	if (d >= depth){
+	  if (type == 1)
+		return value;
+	  else if (type == 0)  // Reducing windows
+		alpha = MAX(alpha,value);
+	  else if(type == 2)
+		beta = MIN(beta,value);
+	if (alpha >= beta) // Null window
+		return alpha;	
+	}
+    }
     
     if (state.terminal() || depth == 0) {
-        negascout_table.insert(make_pair(state, stored_info_t(state.value())));
-        return state.value();
+	if (state.value() <= alpha)
+		type = 0;
+	else if (state.value() >= beta)
+		type = 2;
+	else
+		type = 1;
+	negascout_table.insert(make_pair(state, stored_info_t(state.value(),depth,type)));
+	return state.value();
     }
     
     std::vector<state_t> children = state.getChildren(player);
     int m = MAX(INT_MIN, negaMin(children[0], depth - 1, alpha, beta, !player));
     
     if (m >= beta) {
-        negascout_table.insert(make_pair(state, stored_info_t(m)));
+        negascout_table.insert(make_pair(state, stored_info_t(m,depth,2)));
         return m;
     }
     
@@ -38,31 +57,54 @@ int negaMax(state_t state, int depth, int alpha, int beta, bool player) {
             m = (score >= beta) ? score : negaMin(children[i], depth - 1, score, beta, !player);
         
         if (m >= beta) {
-            negascout_table.insert(make_pair(state, stored_info_t(m)));
+           negascout_table.insert(make_pair(state, stored_info_t(m,depth,2)));
             return m;
         }
+ 	negascout_table.insert(make_pair(state, stored_info_t(m,depth,2)));
     }
     
-    negascout_table.insert(make_pair(state, stored_info_t(m)));
+    negascout_table.insert(make_pair(state, stored_info_t(m,depth,0)));
     return m;
 }
+
+
 
 int negaMin(state_t state, int depth, int alpha, int beta, bool player) {
     hash_table_t::iterator it = negascout_table.find(state);
     
-    if (it != negascout_table.end())
-        return (it->second).val;
+    int value, d, type;
+    if (it != negascout_table.end()){
+	value = (it->second).val; 
+        d = (it->second).depth;
+	type = (it->second).type;
+	if (d >= depth){
+	  if (type == 1)
+		return value;
+	  else if (type == 0)
+		alpha = MAX(alpha,value);
+	  else if(type == 2)
+		beta = MIN(beta,value);
+	if (alpha >= beta)
+		return alpha;	
+	}
+    }
     
     if (state.terminal() || depth == 0) {
-        negascout_table.insert(make_pair(state, stored_info_t(state.value())));
-        return state.value();
+	if (state.value() <= alpha)
+		type = 0;
+	else if (state.value() >= beta)
+		type = 2;
+	else
+		type = 1;
+	negascout_table.insert(make_pair(state, stored_info_t(state.value(),depth,type)));
+	return state.value();
     }
 	
     std::vector<state_t> children = state.getChildren(player);
     int m = MIN(INT_MAX, negaMax(children[0], depth - 1, alpha, beta, !player));
     
     if (m <= alpha) {
-        negascout_table.insert(make_pair(state, stored_info_t(m)));
+        negascout_table.insert(make_pair(state, stored_info_t(m,depth,0)));
         return m;
     }
     
@@ -73,12 +115,13 @@ int negaMin(state_t state, int depth, int alpha, int beta, bool player) {
             m = (score <= alpha) ? score : negaMax(children[i], depth - 1, alpha, score, !player);
         
         if (m <= alpha) {
-            negascout_table.insert(make_pair(state, stored_info_t(m)));
+            negascout_table.insert(make_pair(state, stored_info_t(m,depth,0)));
             return m;
         }
+	 negascout_table.insert(make_pair(state, stored_info_t(m,depth,0)));
     }
     
-    negascout_table.insert(make_pair(state, stored_info_t(m)));
+    negascout_table.insert(make_pair(state, stored_info_t(m,depth,2)));
     return m;
 }
 
